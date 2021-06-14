e524631f56abd81e795f822469dc731d24dc4716
Gilad Novik <gilad@novik.ca>
2021-06-14 19:12:50 +0300

    exchange support

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
 const amqplib = require('amqplib');
 
 module.exports = class RabbitMQ {
-    constructor(url) {
+    constructor(url, exchange) {
         this.queues = {};
         this.channel = (async () => {
             for (;;) {
                 try {
                     log.debug('Connecting to RabbitMQ...');
                     const connection = await amqplib.connect(url);
-                    return await connection.createChannel();
+                    const channel = await connection.createChannel();
+                    if ((this.exchange = exchange)) await channel.assertExchange(exchange, 'x-delayed-message', {arguments: {'x-delayed-type': 'direct'}});
+                    return channel;
                 } catch (err) {
                     if (__DEV__ && err.code == 'ECONNREFUSED') {
                         await new Promise((resolve) => setTimeout(resolve, 1000));
@@ -22,19 +24,21 @@ module.exports = class RabbitMQ {
     }
 
     async assertQueue(queue) {
-        if (!this.queues[queue]) this.queues[queue] = (await this.channel).assertQueue(queue);
+        const channel = await this.channel;
+        if (!this.queues[queue]) this.queues[queue] = channel.assertQueue(queue);
         return await this.queues[queue];
     }
 
     async send(queue, content, {delay} = {}) {
         await this.assertQueue(queue);
-        return await (await this.channel).sendToQueue(queue, Buffer.from(JSON.stringify(content)), delay ? {headers: {'x-delay': delay}} : {});
+        return await (await this.channel).publish(this.exchange || '', queue, Buffer.from(JSON.stringify(content)), delay ? {headers: {'x-delay': delay}} : {});
     }
 
     async consume(queue, handler, {prefetch} = {}) {
         await this.assertQueue(queue);
         const channel = await this.channel;
         if (prefetch) await channel.prefetch(prefetch);
+        if (this.exchange) await channel.bindQueue(queue, this.exchange, queue);
         channel.consume(queue, async (msg) => {
             try {
                 await handler(JSON.parse(msg.content.toString('utf8')));
