ebaa0d646791ef87caf355c50a01555a6731d031
Felix Weizman <felix@zeitgold.com>
2021-09-03 19:29:05 +0200

    dead-letter-exchange

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const EventEmitter = require('events');
 const amqplib = require('amqplib');
 
+const DLX_NAME = 'DeadLetterExchange';
+const DLQ_NAME = 'DeadLetterQueue';
+
 module.exports = class RabbitMQ extends EventEmitter {
     constructor(url, exchange = '') {
         super();
@@ -10,10 +13,21 @@ module.exports = class RabbitMQ extends EventEmitter {
             this.connection = (async () => {
                 for (;;) {
                     try {
-                        log.debug('Connecting to RabbitMQ...');
+                        // log.debug('Connecting to RabbitMQ...');
                         const connection = await amqplib.connect(this.url);
                         this.channel = await connection.createChannel();
-                        if (this.exchange) await this.channel.assertExchange(this.exchange, 'x-delayed-message', {arguments: {'x-delayed-type': 'direct'}});
+
+                        if (this.exchange) {
+                            await this.channel.assertExchange(`${this.exchange}_${DLX_NAME}`, 'fanout');
+
+                            await this.channel.assertQueue(`${this.exchange}_${DLQ_NAME}`, {
+                                arguments: {
+                                    'x-dead-letter-exchange': this.exchange,
+                                    'x-message-ttl': 5000,
+                                },
+                            });
+                            await this.channel.assertExchange(this.exchange, 'x-delayed-message', {arguments: {'x-delayed-type': 'direct'}});
+                        }
                         connection.on('close', (err) => {
                             setImmediate(() => this.emit('close'));
                             if (!err) return;
@@ -40,7 +54,14 @@ module.exports = class RabbitMQ extends EventEmitter {
 
     async assertQueue(queue) {
         await this.connection;
-        if (!this.queues[queue]) this.queues[queue] = this.channel.assertQueue(queue);
+        if (!this.queues[queue])
+            this.queues[queue] = this.channel.assertQueue(queue, {
+                durable: true,
+                autoDelete: false,
+                arguments: {
+                    'x-dead-letter-exchange': `${this.exchange}_${DLX_NAME}`,
+                },
+            });
         return await this.queues[queue];
     }
 
@@ -52,16 +73,20 @@ module.exports = class RabbitMQ extends EventEmitter {
     async consume(queue, handler, {prefetch} = {}) {
         await this.assertQueue(queue);
         if (prefetch) await this.channel.prefetch(prefetch);
-        if (this.exchange) await this.channel.bindQueue(queue, this.exchange, queue);
+        if (this.exchange) {
+            await this.channel.bindQueue(queue, this.exchange, queue);
+            await this.channel.bindQueue(`${this.exchange}_${DLQ_NAME}`, `${this.exchange}_${DLX_NAME}`, '#');
+        }
+
         this.channel.consume(queue, async (msg) => {
             if (!msg) return;
             try {
                 await handler(JSON.parse(msg.content.toString('utf8')));
                 await this.channel.ack(msg);
             } catch (err) {
-                log.error({err, msg: msg.content.toString('utf8')});
+                console.log({err, msg: msg.content.toString('utf8')});
                 if (err instanceof SyntaxError) await this.channel.ack(msg);
-                else await this.channel.nack(msg);
+                else await this.channel.reject(msg, false);
             }
         });
     }
