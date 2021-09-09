6f6a3fccc48bac2585d17a6310bcc6ac6fe3d0b4
Felix Weizman <felix@zeitgold.com>
2021-09-09 14:28:09 +0200

    Revert "Revert "Revert "Dead letter exchange (#2)"""
    This reverts commit fb2ed815be376d073e58a7e2e2be125a058b0ba5.

--- index.js
+++ index.js
@@ -2,13 +2,6 @@
 const EventEmitter = require('events');
 const amqplib = require('amqplib');
 
-const RYX_NAME = 'RetryExchange';
-const RYQ_NAME = 'RetryQueue';
-const RETRY_TIMEOUT = 1000 * 5 * 60;
-
-const SEND_TO_DLQ_AFTER = 5;
-const DLQ_NAME = 'DeadLetterQueue';
-
 module.exports = class RabbitMQ extends EventEmitter {
     constructor(url, exchange = '') {
         super();
@@ -20,19 +13,7 @@ module.exports = class RabbitMQ extends EventEmitter {
                         log.debug('Connecting to RabbitMQ...');
                         const connection = await amqplib.connect(this.url);
                         this.channel = await connection.createChannel();
-
-                        if (this.exchange) {
-                            await this.channel.assertExchange(this.exchange, 'x-delayed-message', {arguments: {'x-delayed-type': 'direct'}});
-
-                            await this.channel.assertExchange(`${this.exchange}_${RYX_NAME}`, 'fanout');
-                            await this.channel.assertQueue(`${this.exchange}_${RYQ_NAME}`, {
-                                arguments: {
-                                    'x-dead-letter-exchange': this.exchange,
-                                    'x-message-ttl': RETRY_TIMEOUT,
-                                },
-                            });
-                            await this.channel.bindQueue(`${this.exchange}_${RYQ_NAME}`, `${this.exchange}_${RYX_NAME}`, '#');
-                        }
+                        if (this.exchange) await this.channel.assertExchange(this.exchange, 'x-delayed-message', {arguments: {'x-delayed-type': 'direct'}});
                         connection.on('close', (err) => {
                             setImmediate(() => this.emit('close'));
                             if (!err) return;
@@ -59,7 +40,7 @@ module.exports = class RabbitMQ extends EventEmitter {
 
     async assertQueue(queue) {
         await this.connection;
-        if (!this.queues[queue]) this.queues[queue] = this.channel.assertQueue(queue, {arguments: {'x-dead-letter-exchange': `${this.exchange}_${RYX_NAME}`}});
+        if (!this.queues[queue]) this.queues[queue] = this.channel.assertQueue(queue);
         return await this.queues[queue];
     }
 
@@ -72,7 +53,6 @@ module.exports = class RabbitMQ extends EventEmitter {
         await this.assertQueue(queue);
         if (prefetch) await this.channel.prefetch(prefetch);
         if (this.exchange) await this.channel.bindQueue(queue, this.exchange, queue);
-
         this.channel.consume(queue, async (msg) => {
             if (!msg) return;
             try {
@@ -80,14 +60,8 @@ module.exports = class RabbitMQ extends EventEmitter {
                 await this.channel.ack(msg);
             } catch (err) {
                 log.error({err, msg: msg.content.toString('utf8')});
-
-                if (!this.exchange || err instanceof SyntaxError || msg.properties?.headers['x-death']?.[0]?.count >= SEND_TO_DLQ_AFTER) {
-                    await this.channel.assertQueue(DLQ_NAME);
-                    await this.channel.publish('', DLQ_NAME, Buffer.from(JSON.stringify({...msg, content: msg.content.toString('utf8')})));
-                    await this.channel.ack(msg);
-                } else {
-                    await this.channel.nack(msg, false, false);
-                }
+                if (err instanceof SyntaxError) await this.channel.ack(msg);
+                else await this.channel.nack(msg);
             }
         });
     }
@@ -99,7 +73,7 @@ module.exports = class RabbitMQ extends EventEmitter {
         delete this.channel;
         delete this.exchange;
         delete this.url;
-        delete this.connection;
+        this.connection = Promise.reject(new Error('RabbitMQ connection is closed'));
         this.queues = {};
     }
 };
